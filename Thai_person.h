66ad4a15adb3f6cfe9681f8e7cbf80767ebe4c19
Camel2548 <Camelmlgxd@gmail.com>
2026-04-01 10:09:51 +0000

    MU

--- Thai_person.h
+++ Thai_person.h
@@ -2,6 +2,11 @@ class Thai_person{
 	private: 
 		long nat_id;
 	public: 
-		Thai_person(int=0);
+		Thai_person(int);
  };
+ 
+ Thai_person::Thai_person(int nid){
+	nat_id=nid;
+	cout<<"Nat Id:"<<nat_id<<endl;
+ }
 
